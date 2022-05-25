2aca25da2e0f642fcb9bdde1ad7a1e576877be83
sandy <sandy@mac.com>
2022-05-26 06:31:39 +1000

    wip

--- main.cpp
+++ main.cpp
@@ -15,13 +15,34 @@ namespace {
 absl::BitGen g_bitgen;
 }
 
-template<typename T,int N>
+template<typename T,size_t N>
 class IndexedDict
 {
 private:
-	std::bitset<N> _lookup;
+	uint32_t _lookup[(N+31)/32]{};
 	std::vector<T> _data;
+public:
+	using const_iterator = typename std::vector<T>::const_iterator;
 
+	size_t size() const { return _data.size(); }
+
+	void emplace( int k, const T &v )
+	{
+		// find the word
+		// find the bit
+		// if set return
+
+	}
+
+	const_iterator end() const
+	{
+		return _data.end();
+	}
+
+	const_iterator find( int k ) const
+	{
+		return _data.end();
+	}
 };
 
 auto randomString( int i_min, int i_max )
@@ -53,30 +74,74 @@ auto print( std::chrono::high_resolution_clock::duration d )
 	return std::to_string( us ) + "us";
 }
 
-template<typename T>
-void test( const char *text, int nb, const std::vector<std::string> &allAttributes )
+void randomizeHeap( std::vector<std::unique_ptr<char[]>> &randomAllocs )
+{
+	auto previousSize = randomAllocs.size();
+	// allocate new
+	auto newAlloc = absl::Uniform<int>( g_bitgen, 2, 10 );
+	for ( int i = 0; i < newAlloc; ++i )
+	{
+		randomAllocs.emplace_back(
+			std::make_unique<char[]>(
+				absl::Uniform<size_t>( g_bitgen, 24, 65536 )
+			));
+	}
+	if ( previousSize == 0 )
+		return;
+
+	auto toDeallocate = std::min( previousSize, absl::Uniform<size_t>( g_bitgen, 1, 5 ) );
+	auto index = absl::Uniform<size_t>( g_bitgen, 0, previousSize - 1 );
+	while ( toDeallocate > 0 )
+	{
+		randomAllocs.erase( randomAllocs.begin() + index );
+		index += 2;
+		if ( index >= randomAllocs.size() )
+			index = 0;
+		--toDeallocate;
+	}
+}
+
+template<typename T, typename RANDOM_KEY>
+void test(
+	const char *text,
+	int nb,
+	const std::vector<std::string> &allAttributes,
+	const RANDOM_KEY &random_key )
 {
 	// test: lookup speed, insert speed, copy speed, memory
 	std::cout << "==========================================" << std::endl;
-	std::cout << text << std::endl;
+	std::cout << text << " " << nb << std::endl;
 
 	std::cout << "sizeof = " << sizeof(T) << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
+	{
+		T container;
+		while ( container.size() < nb )
+		{
+			container.emplace(
+				random_key(),
+				allAttributes[absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 )] );
+		}
+	}
+	auto time = std::chrono::high_resolution_clock::now() - start;
+	std::cout << "creation/destruction time = " << print( time ) << std::endl;
+
+	std::vector<std::unique_ptr<char[]>> randomAllocs;
 	T container;
 	while ( container.size() < nb )
 	{
 		container.emplace(
-			allAttributes[absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 )],
+			random_key(),
 			allAttributes[absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 )] );
+		randomizeHeap( randomAllocs );
 	}
-	auto time = std::chrono::high_resolution_clock::now() - start;
-	std::cout << "creation time = " << print( time ) << std::endl;
+	randomAllocs.clear();
 
 	start = std::chrono::high_resolution_clock::now();
 	int found = 0;
 	for ( int i = 0; i < 1000; ++i )
 	{
-		auto it = container.find(allAttributes[absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 )]);
+		auto it = container.find( random_key() );
 		if ( it != container.end() )
 			++found;
 	}
@@ -87,14 +152,29 @@ void test( const char *text, int nb, const std::vector<std::string> &allAttribut
 
 int main( int argc, char **argv )
 {
-	auto allAttributes = generateAllAttribs( 8, 64, 40000 );
+	auto allAttributes = generateAllAttribs( 8, 64, 800'000 );
 
-	for ( auto nb : { 10, 100, 1000, 10000 } )
+	auto random_string_key = [&]
 	{
-		test<std::map<std::string,std::string>>( "std::map", nb, allAttributes );
-		test<std::unordered_map<std::string,std::string>>( "std::unordered_map", nb, allAttributes );
-		test<absl::btree_map<std::string,std::string>>( "btree_map", nb, allAttributes );
-		test<absl::flat_hash_map<std::string,std::string>>( "flat_hash_map", nb, allAttributes );
-//		test<IndexedDict<std::string,>>( nb, allAttributes );
+		return allAttributes[absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 )];
+	};
+	auto random_int_key = [&]
+	{
+		return absl::Uniform<size_t>( g_bitgen, 0, allAttributes.size() - 1 );
+	};
+
+	test<IndexedDict<std::string,10>>( "IndexedDict", 10, allAttributes, random_int_key );
+	test<IndexedDict<std::string,100>>( "IndexedDict", 100, allAttributes, random_int_key );
+	test<IndexedDict<std::string,1000>>( "IndexedDict", 1000, allAttributes, random_int_key );
+	test<IndexedDict<std::string,100'000>>( "IndexedDict", 100'000, allAttributes, random_int_key );
+
+	for ( auto nb : { 10, 100, 1000, 100'000 } )
+	{
+		test<std::map<std::string,std::string>>( "std::map", nb, allAttributes, random_string_key );
+		test<std::map<int,std::string>>( "std::map2", nb, allAttributes, random_int_key );
+		test<std::unordered_map<std::string,std::string>>( "std::unordered_map", nb, allAttributes, random_string_key );
+		test<absl::btree_map<std::string,std::string>>( "btree_map", nb, allAttributes, random_string_key );
+		test<absl::flat_hash_map<std::string,std::string>>( "flat_hash_map", nb, allAttributes, random_string_key );
 	}
+
 }
